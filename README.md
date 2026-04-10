895330cc20cca5720be669248f165c4ae78849cb
Maxiuts <maximili1008@outlook.com>
2026-04-09 21:48:22 -0600

    yonaguni

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # tikitoki
 script para tiktoks
+
+si quieres usar el codigo para 
+cualquier otra cancion, solo debes de modificar la letra
+
+("Pero ya van par de cervezas\n", 0.06),          lo que hace el numero despues de la letra, es la velocidad 
+
+0.01 - Muy rapido
+0.03 - Velocidad normal
+0.05 - Mas lento
+
+asi es como funciona
+
+time.sleep(0.4)  # pausa entre líneas
+Esta linea es el descanso que hay entre las letras,
+a mayor el numero, mas lenta la continuacion de las letras.
---
+++ yonaguni.py
@@ -0,0 +1,38 @@
+import time
+import sys
+# Colores ANSI
+colores = [
+    "\033[91m",  # rojo
+    "\033[92m",  # verde
+    "\033[93m",  # amarillo
+    "\033[94m",  # azul
+    "\033[95m",  # magenta
+    "\033[96m",  # cyan
+    "\033[97m",  # blanco
+]
+
+reset = "\033[0m"
+
+letras = [
+    ("Vi que viste mi story y subiste una pa' mi\n", 0.07),
+    ("Yo que me iba a dormir, ey\n", 0.05),
+    ("En la disco habían mil\n", 0.05),
+    ("Y yo bailando contigo en mi mente\n", 0.05),
+    ("Aunque sé que no debo\n", 0.08),
+    ("Pensar en ti, bebé, pero cuando bebo\n", 0.09),
+    ("Me viene tu nombre, tu cara, tu risa y tu pelo, ey\n", 0.09),
+    ("Dime donde tu estas que yo por ti cojo un vuelo\n", 0.09),
+    ("Y a Yonaguni le llego\n", 0.09)
+]
+
+def escribir(linea, velocidad, color):
+    print(color, end="")  # aplica color
+    for letra in linea:
+        print(letra, end="", flush=True)
+        time.sleep(velocidad)
+    print(reset, end="")  # resetea color
+
+for i, (linea, velocidad) in enumerate(letras):
+    color = colores[i % len(colores)]  # rota colores
+    escribir(linea, velocidad, color)
+    time.sleep(0.6)  # pausa entre líneas
